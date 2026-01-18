bcc834872c0e14d8dad2b8514a7e5649771a0ebd
AGNAB <133778639+AGNAB712@users.noreply.github.com>
2026-01-17 22:16:50 -0600

    add fs mounts

--- modules/home-server/server.nix
+++ modules/home-server/server.nix
@@ -8,5 +8,14 @@
     enableOnBoot = true;
   };
 
+  fileSystems."/mnt/storage" = {
+    device = "/dev/disk/by-uuid/3962ed81-cdf0-498f-aa3e-30aba2a3e3f1";
+    fsType = "ext4";
+  };
+  fileSystems."/media" = {
+    device = "/dev/disk/by-uuid/3e00a70c-3846-4298-9ec3-24d5cdf4e7b7";
+    fsType = "ext4";
+  };
+
   users.users.agnab.extraGroups = [ "docker" ];
 }
