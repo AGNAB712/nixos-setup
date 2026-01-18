823433f78beb9d6a37b2916e2924891a312d156a
AGNAB <133778639+AGNAB712@users.noreply.github.com>
2026-01-17 21:41:55 -0600

    make me a docker

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -7,6 +7,11 @@
     ../../modules/home-server/server.nix
   ];
 
+  users.users.agnab = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "plugdev" "uucp" "audio" "docker" ];
+  };
+
   networking.hostName = "homeserver";
   time.timeZone = "America/Chicago";
 
