2ffbb8981db4141262964804c59ad211733c25a4
AGNAB <133778639+AGNAB712@users.noreply.github.com>
2026-01-31 16:56:45 -0600

    updated flake for laptop

--- flake.nix
+++ flake.nix
@@ -29,20 +29,15 @@
         inherit system;
         modules = [
 
-          # overlay for quickshell
           ({ pkgs, ... }: {
             nixpkgs.overlays = [
               quickshell.overlays.default
             ];
           })
 
-          # nix-flatpak module
           nix-flatpak.nixosModules.nix-flatpak
-
-          # your system configuration
           ./hosts/desktop/configuration.nix
 
-          # home-manager
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -53,13 +48,35 @@
             home-manager.users.agnab = import ./home/agnab.nix;
           }
 
-          # hyprland config pointing to pinned flake package
           ({ pkgs, ... }: {
             programs.hyprland.enable = true;
             programs.hyprland.package = hyprland.packages.${system}.hyprland;
             programs.hyprland.portalPackage = hyprland.packages.${system}.xdg-desktop-portal-hyprland;
           })
+        ];
+      };
 
+      laptop = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          nix-flatpak.nixosModules.nix-flatpak
+          ./hosts/laptop/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+            };
+            home-manager.users.agnab = import ./home/agnab.nix;
+          }
+
+          ({ pkgs, ... }: {
+            programs.hyprland.enable = true;
+            programs.hyprland.package = hyprland.packages.${system}.hyprland;
+            programs.hyprland.portalPackage = hyprland.packages.${system}.xdg-desktop-portal-hyprland;
+          })
         ];
       };
     };
